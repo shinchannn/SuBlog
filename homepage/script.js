7ca43914fdbc290046fdb5bdbb89cbdf71bab4f4
XIN JIANG <xinjiang@XINs-MacBook-Pro.local>
2020-06-26 09:48:21 -0500

    add folder of homepage design

---
+++ homepage/script.js
@@ -0,0 +1,23 @@
+let navigation = document.getElementById("header");
+let logo = document.getElementById("logo");
+let sticky = navigation.offsetTop;
+
+let middle = document.getElementsByClassName("middle")[0];
+console.log(sticky)
+
+document.body.onscroll = function () {
+  if (window.pageYOffset > sticky) {
+    console.log(document.documentElement.scrollTop);
+
+    navigation.style.height = "70px";
+    navigation.classList.add("sticky");
+    logo.style.fontSize = "2rem";
+    middle.style.paddingTop = "70px";
+  } else {
+    navigation.style.height = "130px";
+    navigation.classList.remove("sticky");
+    logo.style.fontSize = "3rem";
+    middle.style.paddingTop = "0px";
+    
+  }
+};
